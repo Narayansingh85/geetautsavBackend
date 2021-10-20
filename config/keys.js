c0101dbbafbd861b47e45a4745e8784d02843302
Narayan <singhnarayan424@gmail.com>
2021-10-20 10:05:31 +0530

    Adding Git ignore

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-   mongoURI: 'mongodb+srv://ravi:X2PDDVZqftxH2K8P@cluster0.3yica.mongodb.net/umang',
-   semail :  "connecttokrishnanow@gmail.com", // senders email
-   spassword : "krishna1122@",   // senders password
-
-}
