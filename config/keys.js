808aa4d8721bca90c230b12a53c16c2de353e44f
Narayan <singhnarayan424@gmail.com>
2021-11-01 22:32:45 +0530

    Adding config file

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports={
+    mongoURI : 'mongodb+srv://ravi:X2PDDVZqftxH2K8P@cluster0.3yica.mongodb.net/umang?retryWrites=true&w=majority',
+    semail :  "connecttokrishnanow@gmail.com", // senders email
+    spassword : "krishna1122@",   //senders password
+ }
